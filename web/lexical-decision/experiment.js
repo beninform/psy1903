2269632118d6e2d933ca332fbfb038b46c0b4a23
beninform <benwilson5443@yahoo.co.uk>
2025-12-06 17:25:20 +0000

    Lexical decisions - blocks added

--- web/lexical-decision/experiment.js
+++ web/lexical-decision/experiment.js
@@ -6,46 +6,62 @@ let welcomeTrial = {
     type: jsPsychHtmlKeyboardResponse, 
     stimulus: `
     <h1>Welcome to the Lexical Decision Task!</h1> 
-    <p>You are about to see a series of characters.</p>
-    <p>If the characters make up a word, press the F key.</p>
-    <p>If the characters do not make up a word, press the J key.</p>
-    <p>Press SPACE to begin.</p>
+    <p>In this experiment, you will be shown a series of characters and asked to categorize whether the characters make up a word or not.</p>
+    <p>There are three parts to this experiment.</p>
+    <p>Press SPACE to proceed to the first part.</p>
     `,
     choices: [' '], 
 };
 timeline.push(welcomeTrial);
 
-// Create an array of conditions
-let conditions = [
-    { characters: 'cat', isWord: true },
-    { characters: 'pin', isWord: true },
-    { characters: 'jgb', isWord: false },
-    { characters: 'mub', isWord: false },
-];
+// // Create an array of conditions
+// let conditions = [
+//     { characters: 'cat', isWord: true },
+//     { characters: 'pin', isWord: true },
+//     { characters: 'jgb', isWord: false },
+//     { characters: 'mub', isWord: false },
+// ];
 // Shuffle the conditions
 conditions = jsPsych.randomization.repeat(conditions, 1);
 
-for (let condition of conditions) {
-    let conditionTrial = {
+for (let block of conditions) {
+    let blockIntroTrial = {
         type: jsPsychHtmlKeyboardResponse,
-        stimulus: `<h1>${condition.characters}</h1>`,
-        choices: ['f', 'j'],
-        data: {
-        	collect: true, // flag whether we want to collect to csv
-        	characters: condition.characters,
-        },
-        on_finish: function (data) {
-            if (data.response == 'f' && condition.isWord == true) {
-                data.correct = true;
-            } else if (data.response == 'j' && condition.isWord == false) {
-                data.correct = true;
-            } else {
-                data.correct = false;
-            }
-        }
+        stimulus: `
+        	<h1>${block.title}</h1>
+        	<p>You are about to see a series of ${block.count} characters.</p>
+    		<p>If the characters make up a word, press the F key.</p>
+    		<p>If the characters do not make up a word, press the J key.</p>
+    		<p>Press SPACE to begin.</p>
+        	`,
+        choices: [' '],
     };
+    timeline.push(blockIntroTrial);
 
-    timeline.push(conditionTrial);
+    blockConditions = jsPsych.randomization.repeat(block.conditions, 1);
+
+    for (let condition of blockConditions) {
+    	let conditionTrial = {
+    		type: jsPsychHtmlKeyboardResponse,
+    		stimulus: `<h1>${condition.characters}</h1>`,
+    		data: {
+    			collect: true, // flag whether we want to collect to csv
+        		characters: condition.characters,
+        		blockId: block.title,
+	        },
+	        on_finish: function (data) {
+	            if (data.response == 'f' && condition.isWord == true) {
+	                data.correct = true;
+	            } else if (data.response == 'j' && condition.isWord == false) {
+	                data.correct = true;
+	            } else {
+	                data.correct = false;
+	            }
+	        }
+	    };
+
+	    timeline.push(conditionTrial);
+	}
 }
 
 let debriefTrial = {
